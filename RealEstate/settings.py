3e39db04a2ceb77a465d6ebe33c32aa8052dc460
Anandhukrishnan V <Anandhukrishnan.V@gds.ey.com>
2020-12-23 19:44:05 +0530

    on lesson 52

--- RealEstate/settings.py
+++ RealEstate/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -138,3 +139,12 @@ STATICFILES_DIRS=[
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
+
+#Messages
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS={
+    messages.ERROR : 'danger'
+}
